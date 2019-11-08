2934096afb7cb86b7f24a93991247e8b784a034b
kyary4645 <kyary.0426@gmail.com>
2019-11-08 21:05:46 +0900

    initial commit

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,37 @@
+$(function(){
+  function buildHTML(message) {
+    var html = `<div class = message>
+                  <div class = upper-message >
+                    <div class = upper-message__user-name>
+                      ${message.user.name}
+                    </div>
+                    <div class = upper-message__date>
+                      ${message.create_at.strftime("%Y/%m/%d %H:%M")}
+                    </div>
+                  </div>
+                  <div clas =lower-message>
+                  </div>
+                </div>`
+    return html;
+  }
+  $("#new_message").on('submit', function(e){
+    e.preventDefault()
+    console.log(this)
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.message').append(html);
+      $('.form__message').val('');
+      $('.form__submit').prop('disabled', false);
+    })
+  })
+});
