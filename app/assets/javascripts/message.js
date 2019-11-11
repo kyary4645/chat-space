0e41111c0df85def0d2eb9e31bad977153ae4aa2
kyary4645 <kyary.0426@gmail.com>
2019-11-11 09:52:20 +0900

    Desynchronization of message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,25 +1,29 @@
 $(function(){
   function buildHTML(message) {
-    var html = `<div class = message>
+    var image = message.image ? `<img class = lower-message__image, src=${message.image} >` : ' '
+    
+    var html = `<div class = message >
                   <div class = upper-message >
                     <div class = upper-message__user-name>
-                      ${message.user.name}
+                      ${message.user_name}
                     </div>
                     <div class = upper-message__date>
-                      ${message.create_at.strftime("%Y/%m/%d %H:%M")}
+                    ${message.created_at}
                     </div>
                   </div>
                   <div clas =lower-message>
                     <p class = lower-message__content >
                       ${message.content}
                     </p>
+                    ${image}
                   </div>
                 </div>`
+
     return html;
   }
+
   $("#new_message").on('submit', function(e){
     e.preventDefault()
-    console.log(this)
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -32,12 +36,14 @@ $(function(){
     })
     .done(function(data) {
       var html = buildHTML(data);
-      $('.message').append(html);
-      $('.form__message').reset('');
+      $('.messages').append(html);
+      $("#new_message")[0].reset();
       $('.form__submit').prop('disabled', false);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+      $('.form__submit').prop('disabled', false);
     });
   });
 });
